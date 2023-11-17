130c1faae41c28050feacdd185d24b6166045fed
14h02m10s <144254313+14h02m10s@users.noreply.github.com>
2023-11-17 10:57:25 +0100

    modif cv

--- src/components/Presentation2.vue
+++ src/components/Presentation2.vue
@@ -5,7 +5,7 @@ import PresentationItemSkills2 from "./PresentationItemSkills2.vue";
 // const locomotiveScroll = new LocomotiveScroll();
 
 const cvURL =
-  "https://lebrasdejesus.github.io/Portfolio2lbdj/CV-F-Chambinaud-Inte-Dev_compressed.pdf";
+  "https://lebrasdejesus.github.io/Portfolio2lbdj/CV_3W_PF1_compressed.pdf";
 
 // setLocomotiveScroll() {
 //       new LocomotiveScroll({
